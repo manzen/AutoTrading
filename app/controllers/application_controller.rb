4981e2b1c8f8af6489361415cddedf7cbecb4132
manzen <ririn.eva0129@gmail.com>
2018-07-31 19:02:20 +0900

    Basic認証を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic
+
+  private
+  def basic
+    name = 'admin'
+    passwd = 'cdf547ed4c64e6994af35cfcd69c4204c9227a97'
+    authenticate_or_request_with_http_basic('BA') do |n, p|
+      n == name && Digest::SHA1.hexdigest(p) == passwd
+    end
+  end
 end
