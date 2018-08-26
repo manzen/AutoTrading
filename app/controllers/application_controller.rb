ff0e3b28b2dd96221ffc2a3e79bf8871a9e26758
manzen <ririn.eva0129@gmail.com>
2018-08-26 23:13:53 +0900

    basic認証をrailsではしない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic
-
-  private
-  def basic
-    name = Settings.auth.name
-    passwd = Settings.auth.passwd
-    authenticate_or_request_with_http_basic('BA') do |n, p|
-      n == name && Digest::SHA1.hexdigest(p) == passwd
-    end
-  end
 end
